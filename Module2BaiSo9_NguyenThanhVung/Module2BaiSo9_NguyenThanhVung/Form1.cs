f07b80a46ddd4c9a3d145a132c39b71000e49113
Your Name <you@example.com>
2025-03-27 22:22:30 +0700

    Push code

---
+++ Module2BaiSo9_NguyenThanhVung/Module2BaiSo9_NguyenThanhVung/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Module2BaiSo9_NguyenThanhVung
+{
+    public partial class StepByStep2_7 : Form
+    {
+        public StepByStep2_7()
+        {
+            InitializeComponent();
+
+
+            lnkPrograms.Links.Add(0, "Launch Calculator".Length, "calc.exe");
+
+
+            lnkPrograms.Links.Add(lnkPrograms.Text.IndexOf("Open C: Drive"), "Open C: Drive".Length, "c:\\");
+
+
+            lnkPrograms.AutoSize = true;
+        }
+
+        private void lnkWinForms_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+            lnkWinForms.LinkVisited = true;
+
+
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = "http://www.windowsforms.net",
+                UseShellExecute = true
+            });
+        }
+
+        private void lnkPrograms_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+            Process.Start(e.Link.LinkData.ToString());
+        }
+    }
+}
---
+++ Module2BaiSo9_NguyenThanhVung/Module2BaiSo9_NguyenThanhVung/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Module2BaiSo9_NguyenThanhVung")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Module2BaiSo9_NguyenThanhVung")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("21491ee1-c8e3-4848-af05-9ef6c476bab4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
